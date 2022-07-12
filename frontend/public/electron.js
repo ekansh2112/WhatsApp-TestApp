8f1f6bdf7e568e2a1af63cee74d674ed748a26cf
devanshdalmia01 <devanshdalmia1@gmail.com>
2022-07-12 13:48:25 +0530

    electron improved

--- frontend/public/electron.js
+++ frontend/public/electron.js
@@ -1,47 +1,92 @@
-const path = require('path');
+const path = require("path");
+const { app, BrowserWindow, ipcMain, Notification, Tray } = require("electron");
+const isDev = require("electron-is-dev");
 
-const { app, BrowserWindow } = require('electron');
-const isDev = require('electron-is-dev');
+const dockIcon = path.join(__dirname, "Icon.png");
+const trayIcon = path.join(__dirname, "Icon.png");
 
-function createWindow() {
-  // Create the browser window.
-  const win = new BrowserWindow({
-    width: 800,
-    height: 600,
-    webPreferences: {
-      nodeIntegration: true,
-    },
-  });
-
-  // and load the index.html of the app.
-  // win.loadFile("index.html");
-  win.loadURL(
-    isDev
-      ? 'http://localhost:3000'
-      : `file://${path.join(__dirname, '../build/index.html')}`
-  );
-  // Open the DevTools.
-  if (isDev) {
-    win.webContents.openDevTools({ mode: 'detach' });
-  }
+function createSplashWindow() {
+	const win = new BrowserWindow({
+		autoHideMenuBar: true,
+		// width: 1200,
+		// height: 800,
+		frame: false,
+		transparent: true,
+		webPreferences: {
+			nodeIntegration: true,
+			//   worldSafeExecuteJavaScript: true,
+			//   contextIsolation: true,
+		},
+	});
+	win.maximize();
+	win.loadFile("public/splash.html");
+	return win;
 }
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
-app.whenReady().then(createWindow);
+function createWindow() {
+	const win = new BrowserWindow({
+		autoHideMenuBar: true,
+		// width: 1200,
+		// height: 800,
+		// backgroundColor: "#fff",
+		// show: false,
+		webPreferences: {
+			nodeIntegration: true,
+			// worldSafeExecuteJavaScript: true,
+			// contextIsolation: true,
+			preload: path.join(__dirname, "preload.js"),
+		},
+	});
+	win.maximize();
+	win.loadURL(isDev ? "http://localhost:3000" : `file://${path.join(__dirname, "../build/index.html")}`);
+	// if (isDev) {
+	// 	win.webContents.openDevTools({ mode: "detach" });
+	// }
+	return win;
+}
 
-// Quit when all windows are closed, except on macOS. There, it's common
-// for applications and their menu bar to stay active until the user quits
-// explicitly with Cmd + Q.
-app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
-    app.quit();
-  }
+// if (isDev) {
+// 	require("electron-reload")(__dirname, {
+// 		electron: path.join(__dirname, "node_modules", ".bin", "electron"),
+// 	});
+// }
+
+if (process.platform === "darwin") {
+	app.dock.setIcon(dockIcon);
+}
+
+// app.whenReady().then(createWindow);
+
+let tray = null;
+app.whenReady().then(() => {
+	tray = new Tray(trayIcon);
+	const splash = createSplashWindow();
+	const mainApp = createWindow();
+	mainApp.once("ready-to-show", () => {
+		mainApp.hide();
+		setTimeout(() => {
+			splash.destroy();
+			mainApp.show();
+		}, 2000);
+	});
 });
 
-app.on('activate', () => {
-  if (BrowserWindow.getAllWindows().length === 0) {
-    createWindow();
-  }
+ipcMain.on("notify", (_, message) => {
+	new Notification({ title: "Notification", body: message }).show();
+});
+
+ipcMain.on("app-quit", () => {
+	app.quit();
+});
+
+app.on("window-all-closed", () => {
+	if (process.platform !== "darwin") {
+		app.quit();
+	}
+});
+
+app.on("activate", () => {
+	if (BrowserWindow.getAllWindows().length === 0) {
+		createWindow();
+	}
 });
