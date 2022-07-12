b1a6041c6577d1cdc4acccb14ad6207ef0cd800a
aswalasaiteja <62229816+aswalasaiteja@users.noreply.github.com>
2022-07-12 15:50:42 +0530

    webhook

---
+++ webhook/app/message_model.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+
+const messageSchema = new Schema({
+    user_wabaID:{
+        type: String,
+    },
+
+    phoneNumber:{
+        type: String,
+    },
+
+    sent_or_received:{
+        type: String,
+    },
+
+    message_data : {
+		type: mongoose.Schema.Types.Mixed
+    },
+
+    timeStamp:{
+		type: Date,
+		default: Date.now()
+     }
+})
+
+
+module.exports = mongoose.model("Message", messageSchema)
